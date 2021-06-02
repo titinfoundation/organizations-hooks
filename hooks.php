f33a9985ba35e7f9382c381e6279747adeaf60ec
jlugoengi <jlugo.engi@gmail.com>
2021-06-02 13:57:30 -0400

    ssdf

--- hooks.php
+++ hooks.php
@@ -15,7 +15,7 @@ use Directus\Application\Application;
         $item = $item->data;
 
 
-        $oe = use new Directus\Custom\Hooks\Organizations\OrganizationsEmails;
+        $oe = new \Directus\Custom\Hooks\Organizations\OrganizationsEmails();
         $name = "dddsd";
 
         $body = array (
