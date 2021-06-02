ca727542d2afcc929790f57e78b53e11303828dd
jlugoengi <jlugo.engi@gmail.com>
2021-06-01 23:27:59 -0400

    Formated file

--- hooks.php
+++ hooks.php
@@ -4,61 +4,58 @@ use Directus\Application\Application;
 
 return [
   'actions' => [
-    // Post a web callback when an article is created
     'item.update.organizations' => function (array $data) {
 
+      //Access data using item service
+      $container = Application::getInstance()->getContainer();
+      $itemsService = new \Directus\Services\ItemsService($container);
+      $params = ['fields'=>'*.*'];
+      $item = $itemsService->find('organizations', 60, $params);
 
-        $container = Application::getInstance()->getContainer();
-        $itemsService = new \Directus\Services\ItemsService($container);
-
-        $params = ['fields'=>'*.*'];
-        $item = $itemsService->find('organizations', 60, $params);
-
-
-
-      $client = new \GuzzleHttp\Client([
-       'base_uri' => 'https://api.smtp.com'
-      ]);
-
-
-      $myData = array (
+      //Email body
+      $body = array (
         'channel' => 'info_sinfinespr_org',
         'recipients' =>
-        array (
-          'to' =>
           array (
-                0 =>
-                array (
-                  'name' => 'Jorge Lugo',
-                  'address' => 'jlugo.engi@gmail.com',
-                ),
+            'to' =>
+              array (
+                    0 =>
+                    array (
+                      'name' => 'Jorge Lugo',
+                      'address' => 'jlugo.engi@gmail.com',
+                    ),
+              ),
           ),
-        ),
         'originator' =>
-        array (
-                'from' =>
-                array (
-                  'name' =>'SinFines PR',
-                  'address' => 'info@sinfinespr.org',
+          array (
+            'from' =>
+              array (
+                'name' =>'SinFines PR',
+                'address' => 'info@sinfinespr.org',
                 ),
-        ),
+            ),
         'subject' => 'Su organización está bajo revisión pendiente de algunos documentos requeridos',
         'body' =>
         array (
-                'parts' =>
+          'parts' =>
+            array (
+                0 =>
                 array (
-                        0 =>
-                        array (
-                                'type' => 'text/html',
-                                'content' => "Saludos {$item['data']['name']} esto es una prueba. ",
-                                ),
-                        ),
-        ),
-    );
+                  'type' => 'text/html',
+                  'content' => "Saludos {$item['data']['name']} esto es una prueba. ",
+                  ),
+              ),
+          ),
+      );
 
+      //Request to smtp.com api
+      $client = new \GuzzleHttp\Client([
+        'base_uri' => 'https://api.smtp.com'
+      ]);
       $response = $client->request('POST', 'v4/messages?api_key=fe1788dd32593bbc21fa941018856731f3b00f30', [
-        'json' => $myData
-        ]);
+        'json' => $body
+      ]);
+
    }
  ]
 ];
