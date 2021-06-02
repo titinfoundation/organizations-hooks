1776693120642ce6f727e3159012c2c73e6d51cc
jlugoengi <jlugo.engi@gmail.com>
2021-06-02 13:30:11 -0400

    asdffasd

--- hooks.php
+++ hooks.php
@@ -1,7 +1,7 @@
 <?php
 
 use Directus\Application\Application;
-include("./update-email.php");  
+require "./update-email.php";  
   
   
   return [
--- update-email.php
+++ update-email.php
@@ -1,9 +1,9 @@
 <?php
 
-  function updateEmail() {
-    
-    return "mere wowwwwwww";
-  }
+function updateEmail() {
+
+  return "mere wowwwwwww";
+}
 ?>
 
         
