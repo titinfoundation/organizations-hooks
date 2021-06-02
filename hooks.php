920c975f5780e7f58ce1d27960988535c6e0369a
jlugoengi <jlugo.engi@gmail.com>
2021-06-02 14:20:22 -0400

    sdff

--- hooks.php
+++ hooks.php
@@ -1,8 +1,46 @@
 <?php
 
-
 use Directus\Application\Application;
-include "./OrganizationsEmails.php";
+
+  function emailBuilder(){
+
+    $body = array (
+      'channel' => 'info_sinfinespr_org',
+      'recipients' =>
+        array (
+          'to' =>
+            array (
+                  0 =>
+                  array (
+                    'name' => 'Jorge Lugo',
+                    'address' => 'jlugo.engi@gmail.com',
+                  ),
+            ),
+        ),
+      'originator' =>
+        array (
+          'from' =>
+            array (
+              'name' =>'SinFines PR',
+              'address' => 'info@sinfinespr.org',
+              ),
+          ),
+      'subject' => 'Update Su organización está bajo revisión pendiente de algunos documentos requeridos',
+      'body' =>
+      array (
+        'parts' =>
+          array (
+              0 =>
+              array (
+                'type' => 'text/html',
+                'content' => "Saludos sub sup esto es una prueba. ",
+                ),
+            ),
+        ),
+    );
+
+    return $body;
+  }
 
   return [
     'actions' => [
@@ -16,47 +54,9 @@ include "./OrganizationsEmails.php";
 
         $item = $item->data;
 
+        
+        $body = emailBuilder();
 
-        $oe = new OrganizationsEmails();
-        
-        $name = "dddsd";
-
-        $body = array (
-          'channel' => 'info_sinfinespr_org',
-          'recipients' =>
-            array (
-              'to' =>
-                array (
-                      0 =>
-                      array (
-                        'name' => 'Jorge Lugo',
-                        'address' => 'jlugo.engi@gmail.com',
-                      ),
-                ),
-            ),
-          'originator' =>
-            array (
-              'from' =>
-                array (
-                  'name' =>'SinFines PR',
-                  'address' => 'info@sinfinespr.org',
-                  ),
-              ),
-          'subject' => 'Update Su organización está bajo revisión pendiente de algunos documentos requeridos',
-          'body' =>
-          array (
-            'parts' =>
-              array (
-                  0 =>
-                  array (
-                    'type' => 'text/html',
-                    'content' => "Saludos sub sup {$name} esto es una prueba. ",
-                    ),
-                ),
-            ),
-        );
-        
-        
         //Request to smtp.com api
         $client = new \GuzzleHttp\Client([
           'base_uri' => 'https://api.smtp.com'
