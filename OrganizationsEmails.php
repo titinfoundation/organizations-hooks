55b3e32a4005eee749e39a23defdff4cc4dfb470
jlugoengi <jlugo.engi@gmail.com>
2021-06-02 14:07:42 -0400

    asdff

---
+++ OrganizationsEmails.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Directus\Custom\Hooks\Organizations;
+
+
+class OrganizationsEmails 
+{
+  public function handle(array $data)
+  {
+      // set the product sku before insert
+      // $payload->set('sku', 'value');
+
+      // make sure to return the payload
+      return "THis is a test from classs";
+  }
+}
+
+?>
+
+
+
+
+
+
+
