0947dab85d3450bbbf78142d546151046bd568ca
jlugoengi <jlugo.engi@gmail.com>
2021-06-02 01:24:49 -0400

    ddd

--- hooks.php
+++ hooks.php
@@ -12,7 +12,41 @@
         $params = ['fields'=>'*.*'];
         $item = $itemsService->find('organizations', 60, $params);
 
-        $body = updateEmail($item);
+        //$body = updateEmail($item);
+        $body = array (
+          'channel' => 'info_sinfinespr_org',
+          'recipients' =>
+            array (
+              'to' =>
+                array (
+                      0 =>
+                      array (
+                        'name' => 'Jorge Lugo',
+                        'address' => 'jlugo.engi@gmail.com',
+                      ),
+                ),
+            ),
+          'originator' =>
+            array (
+              'from' =>
+                array (
+                  'name' =>'SinFines PR',
+                  'address' => 'info@sinfinespr.org',
+                  ),
+              ),
+          'subject' => 'Update Su organización está bajo revisión pendiente de algunos documentos requeridos',
+          'body' =>
+          array (
+            'parts' =>
+              array (
+                  0 =>
+                  array (
+                    'type' => 'text/html',
+                    'content' => "Saludos sub sup esto es una prueba. ",
+                    ),
+                ),
+            ),
+        );
 
         //Request to smtp.com api
         $client = new \GuzzleHttp\Client([
