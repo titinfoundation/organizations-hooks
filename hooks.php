962e893d06b6f134998f8c39ab02dc25b4ba2e0d
jlugoengi <jlugo.engi@gmail.com>
2021-06-02 01:12:51 -0400

    sss

--- hooks.php
+++ hooks.php
@@ -1,32 +1,31 @@
 <?php
+  include './update-email.php';
+  use Directus\Application\Application;
 
-use Directus\Application\Application;
-include './update-email.php';
+  return [
+    'actions' => [
+      'item.update.organizations' => function (array $data) {
 
-return [
-  'actions' => [
-    'item.update.organizations' => function (array $data) {
+        //Access data using item service
+        $container = Application::getInstance()->getContainer();
+        $itemsService = new \Directus\Services\ItemsService($container);
+        $params = ['fields'=>'*.*'];
+        $item = $itemsService->find('organizations', 60, $params);
 
-      //Access data using item service
-      $container = Application::getInstance()->getContainer();
-      $itemsService = new \Directus\Services\ItemsService($container);
-      $params = ['fields'=>'*.*'];
-      $item = $itemsService->find('organizations', 60, $params);
+        //$body = null;
+        $body = updateEmail($item['data']);
 
-      //$body = null;
-      $body = updateEmail($item['data']);
+        
 
-      
+        //Request to smtp.com api
+        $client = new \GuzzleHttp\Client([
+          'base_uri' => 'https://api.smtp.com'
+        ]);
+        $response = $client->request('POST', 'v4/messages?api_key=fe1788dd32593bbc21fa941018856731f3b00f30', [
+          'json' => $body
+        ]);
 
-      //Request to smtp.com api
-      $client = new \GuzzleHttp\Client([
-        'base_uri' => 'https://api.smtp.com'
-      ]);
-      $response = $client->request('POST', 'v4/messages?api_key=fe1788dd32593bbc21fa941018856731f3b00f30', [
-        'json' => $body
-      ]);
-
-   }
- ]
-];
+    }
+  ]
+  ];
         
