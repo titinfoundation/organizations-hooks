f50fe5f6ede72bc109db986089cd724502c77253
jlugoengi <jlugo.engi@gmail.com>
2021-06-02 01:11:08 -0400

    aa

--- update-email.php
+++ update-email.php
@@ -2,40 +2,40 @@
 
   function updateEmail(array $data) {
 
-      $body = array (
-        'channel' => 'info_sinfinespr_org',
-        'recipients' =>
-          array (
-            'to' =>
-              array (
-                    0 =>
-                    array (
-                      'name' => 'Jorge Lugo',
-                      'address' => 'jlugo.engi@gmail.com',
-                    ),
+    $body = array (
+      'channel' => 'info_sinfinespr_org',
+      'recipients' =>
+        array (
+          'to' =>
+            array (
+                  0 =>
+                  array (
+                    'name' => 'Jorge Lugo',
+                    'address' => 'jlugo.engi@gmail.com',
+                  ),
+            ),
+        ),
+      'originator' =>
+        array (
+          'from' =>
+            array (
+              'name' =>'SinFines PR',
+              'address' => 'info@sinfinespr.org',
               ),
           ),
-        'originator' =>
+      'subject' => 'Update Su organización está bajo revisión pendiente de algunos documentos requeridos',
+      'body' =>
+      array (
+        'parts' =>
           array (
-            'from' =>
+              0 =>
               array (
-                'name' =>'SinFines PR',
-                'address' => 'info@sinfinespr.org',
+                'type' => 'text/html',
+                'content' => "Saludos sub sup esto es una prueba. ",
                 ),
             ),
-        'subject' => 'Update Su organización está bajo revisión pendiente de algunos documentos requeridos',
-        'body' =>
-        array (
-          'parts' =>
-            array (
-                0 =>
-                array (
-                  'type' => 'text/html',
-                  'content' => "Saludos sub sup {$data['name']} esto es una prueba. ",
-                  ),
-              ),
-          ),
-      );
+        ),
+    );
 
     return $body;
   }
