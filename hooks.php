cc67ad216be8d1adbac3e13ee491306985eb4dad
jlugoengi <jlugo.engi@gmail.com>
2021-08-09 22:29:01 -0400

    pppppp

--- hooks.php
+++ hooks.php
@@ -9,7 +9,7 @@ use Directus\Application\Application;
         $other_assets = $payload->get('other_assets');
         $income_total = $payload->get('income_total');
 
-       if(is_null($other_assets)||true){
+       if(is_null($other_assets)||is_null($income_total)){
           //Access data using item service
           $container = Application::getInstance()->getContainer();
           $itemsService = new \Directus\Services\ItemsService($container);
